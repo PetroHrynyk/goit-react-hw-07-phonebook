d526b83d35f0902a79d135833caf678815311dbc
PetroHrynyk <grynyk82@gmail.com>
2022-11-16 17:52:15 +0200

    add loader

--- src/Redux/selectors.jsx
+++ src/Redux/selectors.jsx
@@ -1,2 +1,4 @@
 export const getContacts = ({ contacts }) => contacts.items;
 export const getFilteredContact = ({ contacts }) => contacts.filter; 
+export const getLoading = ({contacts}) => contacts.loading;
+export const getError = ({contacts}) => contacts.error;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,19 @@ import ContactForm from 'components/ContactForm/ContactForm';
 import Filter from 'components/Filter/Filter';
 import ContactList from 'components/ContactList/ContactList';
 
+import { useSelector } from 'react-redux';
+import { getError, getLoading } from 'Redux/selectors';
+
 export default function App() {
+  const loading = useSelector(getLoading);
+  const error = useSelector(getError);
   return (
     <div className={style.main}>
       <h2 className={style.title}>Phonebook</h2>
       <ContactForm />
       <h3 className={style.title}>Contacts</h3>
       <Filter />
+      {loading && !error && <b>Request in progress...</b>}
       <ContactList />
     </div>
   );
